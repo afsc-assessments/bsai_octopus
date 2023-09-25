38bbc1cf44110f4988c62216b812f4a6ff56daea
Ben <ben.williams@noaa.gov>
2023-09-25 10:58:42 -0800

    analysis

--- 2023/R/2023_analysis.R
+++ 2023/R/2023_analysis.R
@@ -4,13 +4,15 @@
 # ben.williams@noaa.gov
 
 # load ----
-# devtools::unload("afscdata")
+# remotes::install_github("BenWilliams-NOAA/afscassess")
+# remotes::install_github("afsc-assessments/afscdata")
 library(afscdata)
 library(afscassess)
+library(ggplot2)
+theme_set(theme_report())
 
 # globals ----
 year = 2023
-species = 870 # can't use species group code 'OTHR' as multiple species in there
 
 # setup folder structure - only run this once
 # setup_folders(year)
@@ -18,6 +20,8 @@ species = 870 # can't use species group code 'OTHR' as multiple species in there
 # query data ----
 bsai_octopus(year)
 
+# cleanup catch data 
+# afscdata is not currently setup for bsai octopus catch data so add in archived data here (it came from SAFE doc, no idea on origin...)
 
 old_catch <- data.frame(year = 1997:2002,
                        catch = c(160,168,310,359,211,334,
@@ -29,7 +33,7 @@ old_catch <- data.frame(year = 1997:2002,
 
 vroom::vroom(here::here(year, "data", "raw", "fsh_catch_data.csv")) %>%  
   tidytable::filter(year >= 2003) %>%
-  tidytable::mutate(target = case_when(trip_target_code=="C" ~ "pcod",
+  tidytable::mutate(target = tidytable::case_when(trip_target_code=="C" ~ "pcod",
                             !(trip_target_code %in% c("A", "O", "C", "I", "B", "P", "K", "S")) ~ "flatfish",
                             TRUE ~ "other")) %>% 
   tidytable::summarise(catch = sum(weight_posted, na.rm = T),
@@ -45,34 +49,63 @@ vroom::vroom(here::here(year, "data", "raw", "fsh_catch_data.csv")) %>%
 
 vroom::vroom_write(catch, here::here(year, "data", "output", "catch.csv"))
 
-  
-db = connect()
+# cleanup survey data   
+vroom::vroom(here::here(year, "data", "raw", "survey_biomass.csv")) %>% 
+  tidytable::summarise(biomass = sum(biomass_mt),
+                       var = sum(biomass_var, na.rm=T),
+                       .by = c(year, area_id )) -> srv
+# survey table
+srv %>% 
+  tidytable::mutate(cv = sqrt(var) / biomass * 100) %>% 
+  tidytable::mutate(area = tidytable::case_when(area_id==99900 ~ "EBS_shelf",
+                                                area_id==99905 ~ "EBS_slope",
+                                                area_id==99904 ~ "AI")) %>% 
+  tidytable::pivot_wider(names_from = area, values_from = c(biomass, cv)) %>% 
+  tidytable::select(-area_id, -var) -> survey
 
-table = dplyr::tbl(db, dplyr::sql("afsc.race_biomass_ebsshelf_plusnw"))
-dplyr::collect(table)
+vroom::vroom_write(survey, here::here(year, "data", "output", "bts_biomass.csv"))
 
-
-afsc_species = c(78010, 78011, 78012, 78013, 78020, 78021, 78022, 78023, 
-                 78030, 78040, 78210, 78300, 78301, 78352, 78353, 78403, 
-                 78404, 78452, 78454, 78455)
-afsc_species = c(78010, 78012, 78020, 78403, 78454, 78455)
-
-db2 = connect("afsc")
-dplyr::tbl(db2, dplyr::sql("gap_products.biomass")) %>% 
-  dplyr::rename_with(tolower) %>% 
-  dplyr::filter(area_id==99900, species_code %in% afsc_species) %>% 
-  dplyr::group_by(year, species_code) %>% 
-  dplyr::summarise(biomass = sum(biomass_mt)) %>% 
-  dplyr::collect() %>% 
-  dplyr::arrange(species_code, year) %>% 
-  View()
+# plots 
+dir.create(here::here(year, "figs"))
 
 
-table %>% 
-  dplyr::rename_with(tolower) %>% 
-  dplyr::filter(species_code %in% afsc_species, species_name=="Enteroctopus dofleini") %>% 
-  dplyr::group_by(year, species_name) %>% 
-  dplyr::summarise(biomass = sum(biomass)) %>% dplyr::arrange(year) %>% View()
+png(filename=here::here(year, "figs", "catch.png"), width = 6.5, height = 6.5,
+    units = "in", type ="cairo", res = 200)
+
+catch %>% 
+  ggplot(aes(year, total)) + 
+  geom_point() +
+  geom_line() +
+  expand_limits(y=0) +
+  xlab("Year") +
+  ylab("Catch (t)") +
+  afscassess::scale_x_tickr(data = catch, var=year)
+
+dev.off()
+
+
+
+png(filename=here::here(year, "figs", "survey.png"), width = 6.5, height = 6.5,
+    units = "in", type ="cairo", res = 200)
+
+srv %>% 
+  tidytable::filter(year>1986) %>% 
+  tidytable::mutate(area = tidytable::case_when(area_id==99900 ~ "EBS_shelf",
+                                                area_id==99905 ~ "EBS_slope",
+                                                area_id==99904 ~ "AI"),
+                    lci = biomass - sqrt(var) * 1.96,
+                    uci = biomass + sqrt(var) * 1.96,
+                    lci = ifelse(lci<0, 0, lci)) %>% 
+  ggplot(aes(year, biomass, color=area)) +
+  geom_point(position=position_dodge(width=0.6)) + 
+  geom_errorbar(aes(ymin=lci, ymax=uci), width=0.2, position=position_dodge(width=0.6)) +
+  scico::scale_color_scico_d("Area", palette = 'roma', begin = 0.2) +
+  afscassess::scale_x_tickr(data = srv, var=year) +
+  xlab("Year") +
+  ylab("Biomass (t)") +
+  theme(legend.position = c(.9, .8))
+
+dev.off()
 
 
 
